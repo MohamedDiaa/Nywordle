aeab5eec6315696a58b5f5440e3037ff9d86c492
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-27 12:03:23 +0200

    Initial Commit

---
+++ Nywordle/NywordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NywordleApp.swift
+//  Nywordle
+//
+//  Created by Mohamed Alwakil on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct NywordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
