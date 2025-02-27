2c475ff2ce526ed891d45d1978934aa3ec2a4f39
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-02-28 01:20:04 +0200

    add keyboard to mainview

--- Nywordle/MainView.swift
+++ Nywordle/MainView.swift
@@ -7,7 +7,19 @@
 
 import SwiftUI
 
+struct CellValue: Identifiable, Hashable {
+    var id = UUID.init().uuidString
+    var value: String
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
 struct MainView: View {
+
+    @State var row: Int = 0
+    @State var column: Int = 0
+    @State var words = createCells()
 
     var body: some View {
 
@@ -15,43 +27,83 @@ struct MainView: View {
             ZStack {
                 Color.black.opacity(0.7).mix(with: .blue, by: 0.3)
                     .ignoresSafeArea()
-                LazyVGrid(columns: [GridItem(),GridItem(),GridItem(),GridItem()], alignment: .center, spacing: 0) {
 
-                    ForEach(0..<6) { _ in
-                        ForEach(0..<4) { _ in
-                            let width = proxy.size.width / 4
-                            cellView(width: width)
+                VStack {
+                    LazyVGrid(columns: [GridItem(),GridItem(),GridItem(),GridItem(),GridItem()], alignment: .center, spacing: 0) {
 
+                        ForEach(words, id: \.self) { word in
+
+                            ForEach(word, id: \.id) {cellValue in
+                                let width = proxy.size.width / 5
+                                cellView(char: String(cellValue.value),width: width)
+
+                            }
                         }
                     }
-                }
-                .padding()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .padding()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
 
+                    keyboard()
+                }
             }
         }
 
     }
 
     @ViewBuilder
-    func cellView(width: CGFloat) -> some View {
+    func cellView(char: String, width: CGFloat) -> some View {
         Color.white
             .font(.system(size: 60))
             .padding(10)
             .foregroundStyle(.white)
             .background(Rectangle().fill(.black).border(.blue, width: 1))
             .frame(width: width, height: 80)
+            .overlay {
+                Text(char)
+                    .font(.system(size: 36))
+            }
 
     }
 
+    let alphabets: [String] = ["A","B","C","D","E",
+                     "F","G","H","I","J",
+                     "K","L","M","N","O",
+                     "P","Q","R","S","T",
+                     "U","V","W","X","Y",
+                     "Z","Enter","🔙"]
 
     @ViewBuilder
-    func key() -> some View {
-        Text("A")
+    func keyboard() -> some View {
+        LazyVGrid(columns: Array.init(repeating: GridItem(), count: 10),spacing: 10) {
+
+            ForEach(alphabets, id: \.self) { char in
+                key(char: char)
+            }
+        }
+    }
+
+    @ViewBuilder
+    func key(char: String) -> some View {
+        Text("\(char)")
+            .padding()
             .background(.gray.opacity(0.5))
             .foregroundStyle(.white)
             .bold()
-            .frame(width: 50, height: 50)
+            .frame(width: 100)
+            .onTapGesture {
+
+                if row < 6, column < 5 {
+                    words[row][column].value = char
+                    column += 1
+                }
+            }
+    }
+
+    static func createCells() -> [[CellValue]] {
+
+        return (0..<6).map { _ in
+            (0..<5).map { _ in CellValue(value: "") }
+        }
     }
 }
 
